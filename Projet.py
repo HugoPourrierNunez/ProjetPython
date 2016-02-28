702c2cb120f225f5286157a9f47666b253ba2e31
HugoPourrierNunez <pourrier789@gmail.com>
2016-02-28 21:09:57 +0100

    Un premier aperçu de la lumière diffuse

--- Projet.py
+++ Projet.py
@@ -7,6 +7,9 @@ class Point:
         self.x=x
         self.y=y
         self.z=z
+
+    def norme(self,p):
+        return sqrt((p.x-self.x)**2+(p.y-self.y)**2+(p.z-self.z)**2)
         
 class Vector:
 
@@ -14,7 +17,12 @@ class Vector:
         self.x=x
         self.y=y
         self.z=z
-        
+
+    def myprint(self):
+        print("x=",self.x," y=",self.y," z=",self.z)
+
+    def scalaire(self,v):
+        return self.x*v.x+self.y*v.y+self.z*v.z
 
 class Color:
 
@@ -69,7 +77,6 @@ class Scene:
         self.camera=camera
         self.tabLight=[]
         self.tabSphere=[]
-        self.buffer=[(0,0,0)]*self.camera.screen_width*self.camera.screen_height
         self.image = Image.new("RGB", (self.camera.screen_width, self.camera.screen_height))
         self.buffer = self.image.load()
         for x in range(self.camera.screen_width):
@@ -83,44 +90,67 @@ class Scene:
             self.tabSphere.append(element)
 
     def process(self):
-        for light in self.tabLight:
-            if light.mode == Light.AMBIANTE:
-                direction=Vector(0,1,0)
-                for i in range(0,self.camera.screen_width):
-                    for j in range(0,self.camera.screen_height):
-                        #direction=Vector(i-position.x,j-position.y,self.camera.focale-position.z)
-                        #print("x=",direction.x,"y=",direction.y,"z=",direction.z)
-                        position=Vector(i,j,0)
-                        pointMin = False
-                        colorMin = False
-                        for sphere in self.tabSphere:
-                            find=False
-                            a = direction.x**2 + direction.y**2 + direction.y**2
-                            b= 2*(direction.x*(position.x-sphere.point.x)+direction.y*(position.y-sphere.point.y)+direction.z*(position.z-sphere.point.z))
-                            c = ((sphere.point.x-position.x)**2+(sphere.point.y-position.y)**2+(sphere.point.z-position.z)**2)-sphere.rayon**2
-                            det = b**2-(4*a*c)
-                            if det>0:
-                                t1 = (-b+sqrt(det))/(2*a)
-                                t2 = (-b-sqrt(det))/(2*a)
-                                if abs(t1)>abs(t2):
-                                    t1=t2
-                                find=t1
-                            elif det==0:
-                                find=-b/(2*a)
-                            if find!=False:
-                                p = Point(((position.x+direction.x*find)-sphere.point.x)**2, ((position.y+direction.y*find)-sphere.point.y)**2,((position.z+direction.z*find)-sphere.point.z)**2)
-                                if pointMin==False:
-                                    normeMin=sqrt((p.x-position.x)**2+(p.y-position.y)**2+(p.z-position.z)**2)
-                                    pointMin=p
-                                    colorMin=sphere.color
-                                else:
-                                    norme=sqrt((p.x-position.x)**2+(p.y-position.y)**2+(p.z-position.z)**2)
-                                    if norme<normeMin:
-                                        normeMin=norme
-                                        pointMin=p
-                                        colorMin=sphere.color
-                        if pointMin!=False:
-                            self.buffer[i,j]=colorMin.getTuple()
+        direction=Vector(0,1,0)
+        maxI=0
+        for i in range(0,self.camera.screen_width):
+            for j in range(0,self.camera.screen_height):
+                position=Vector(i,j,0)
+                pointMin = False
+                colorMin = False
+                for sphere in self.tabSphere:
+                    find=False
+                    a = direction.x**2 + direction.y**2 + direction.y**2
+                    b= 2*(direction.x*(position.x-sphere.point.x)+direction.y*(position.y-sphere.point.y)+direction.z*(position.z-sphere.point.z))
+                    c = ((sphere.point.x-position.x)**2+(sphere.point.y-position.y)**2+(sphere.point.z-position.z)**2)-sphere.rayon**2
+                    det = b**2-(4*a*c)
+                    if det>0:
+                        t1 = (-b+sqrt(det))/(2*a)
+                        t2 = (-b-sqrt(det))/(2*a)
+                        if t1>t2:
+                            t1=t2
+                        find=t1
+                    elif det==0:
+                        find=-b/(2*a)
+                    if find!=False:
+                        p=Point(direction.x*find+position.x,direction.y*find+position.y,direction.z*find+position.z)
+                        if pointMin==False:
+                            normeMin=p.norme(position)
+                            pointMin=p
+                            sphereMin=sphere
+                        else:
+                            norme=p.norme(position)
+                            if norme<normeMin:
+                                normeMin=norme
+                                pointMin=p
+                                sphereMin=sphere
+
+                if pointMin!=False:
+                    
+                    for light in self.tabLight:
+                        
+                        if light.mode == Light.DIFFUSE:
+                            
+                            normeL=p.norme(light.point)
+                            
+                            L=Vector((light.point.x-p.x)/normeL,(light.point.y-p.y)/normeL,(light.point.z-p.z)/normeL)
+                            N=Vector((p.x-sphereMin.point.x)/sphereMin.rayon,(p.y-sphereMin.point.y)/sphereMin.rayon,(p.z-sphereMin.point.z)/sphereMin.rayon)
+                            
+                            intensite=L.scalaire(N)*sphereMin.diffuse*light.intensite
+                            if intensite>maxI:
+                                maxI=intensite
+                            if intensite>0:
+                                self.buffer[i,j]=(self.buffer[i,j][0]+int(sphereMin.color.r*intensite),self.buffer[i,j][1]+int(sphereMin.color.g*sphereMin.diffuse*intensite),self.buffer[i,j][2]+int(sphereMin.color.b*sphereMin.diffuse*intensite))
+                            
+                        if light.mode == Light.AMBIANTE:
+                            self.buffer[i,j]=(self.buffer[i,j][0]+int(sphereMin.color.r*sphereMin.ambiante),self.buffer[i,j][1]+int(sphereMin.color.g*sphereMin.ambiante),self.buffer[i,j][2]+int(sphereMin.color.b*sphereMin.ambiante))
+
+                        if light.mode == Light.SPECULAIRE:
+                            test=1
+                        
+
+        print("maxI",maxI)
+        
+
             
     def process2(self):
         test=1
@@ -134,9 +164,10 @@ class Scene:
 
     
 scene = Scene(Camera(500,500,500,500,1000))
-scene+Sphere(Point(250,250,0), 50, Color(0,0,255))
-scene+Sphere(Point(200,250,10), 100, Color(255,0,0))
-scene+Light(Point(10,20,5))
+scene+Sphere(Point(250,250,-25), 50, Color(0,0,255),ambiante=.2)
+scene+Sphere(Point(200,250,10), 100, Color(255,0,0),ambiante=.2)
+scene+Light(Point(0,0,0), mode=Light.DIFFUSE)
+scene+Light(Point(0,0,0))
 scene.process()
 scene.draw("test2.jpg")
 
