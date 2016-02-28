726e477190f2df48e84beae1d803dc91b170c149
HugoPourrierNunez <pourrier789@gmail.com>
2016-02-28 14:35:38 +0100

    Détection de la sphere la plus proche de la camera

--- Projet.py
+++ Projet.py
@@ -22,6 +22,9 @@ class Color:
         self.b=b
         self.r=r
         self.g=g
+        
+    def getTuple(self):
+        return (self.r,self.g,self.b)
 
 
 class Ray:
@@ -41,16 +44,24 @@ class Camera:
         
 
 class Light:
-    def __init__(self,point,color):
+    AMBIANTE=1
+    DIFFUSE=2
+    SPECULAIRE=3
+    def __init__(self,point,color=Color(),intensite=1, mode=AMBIANTE):
         self.point=point
         self.color=color
+        self.mode=mode
+        self.intensite=intensite
 
 class Sphere:
-    def __init__(self,point,rayon,color,coef=1):
+    def __init__(self,point,rayon,color,ambiante=1,diffuse=1,speculaire=1,brillance=1):
         self.point=point
         self.rayon=rayon
         self.color=color
-        self.coef=coef
+        self.ambiante = ambiante
+        self.diffuse = diffuse
+        self.speculaire=speculaire
+        self.brillance=brillance
 
 class Scene:
     
@@ -59,6 +70,11 @@ class Scene:
         self.tabLight=[]
         self.tabSphere=[]
         self.buffer=[(0,0,0)]*self.camera.screen_width*self.camera.screen_height
+        self.image = Image.new("RGB", (self.camera.screen_width, self.camera.screen_height))
+        self.buffer = self.image.load()
+        for x in range(self.camera.screen_width):
+            for y in range(self.camera.screen_height):
+                self.buffer[x,y]=(0,0,0)
                      
     def __add__(self, element):
         if isinstance(element,Light):
@@ -67,49 +83,60 @@ class Scene:
             self.tabSphere.append(element)
 
     def process(self):
-        direction=Vector(0,1,0)
-        for i in range(0,self.camera.screen_width):
-            for j in range(0,self.camera.screen_height):
-                position=Point(i,j,0)
-                pointMin = False
-                colorMin = False
-                for sphere in self.tabSphere:
-                    find=False
-                    a = direction.x**2 + direction.y**2 + direction.y**2
-                    b= 2*(direction.x*(position.x-sphere.point.x)+direction.y*(position.y-sphere.point.y)+direction.z*(position.z-sphere.point.z))
-                    c = ((sphere.point.x-position.x)**2+(sphere.point.y-position.y)**2+(sphere.point.z-position.z)**2)-sphere.rayon**2
-                    det = b**2-(4*a*c)
-                    if det>0:
-                        t1 = (-b+sqrt(det))/(2*a)
-                        t2 = (-b-sqrt(det))/(2*a)
-                        if abs(t1)>abs(t2):
-                            t1=t2
-                        find=t1
-                    elif det==0:
-                        find=-b/(2*a)
-                    if find!=False:
-                        p = Point(((position.x+direction.x*find)-sphere.point.x)**2, ((position.y+direction.y*find)-sphere.point.y)**2,((position.z+direction.z*find)-sphere.point.z)**2)
-                        pointMin=p
-                        colorMin=sphere.color
-                if pointMin!=False:
-                    self.buffer[i*self.camera.screen_width+j]=(colorMin.r,colorMin.g,colorMin.b)
+        for light in self.tabLight:
+            if light.mode == Light.AMBIANTE:
+                direction=Vector(0,1,0)
+                for i in range(0,self.camera.screen_width):
+                    for j in range(0,self.camera.screen_height):
+                        #direction=Vector(i-position.x,j-position.y,self.camera.focale-position.z)
+                        #print("x=",direction.x,"y=",direction.y,"z=",direction.z)
+                        position=Vector(i,j,0)
+                        pointMin = False
+                        colorMin = False
+                        for sphere in self.tabSphere:
+                            find=False
+                            a = direction.x**2 + direction.y**2 + direction.y**2
+                            b= 2*(direction.x*(position.x-sphere.point.x)+direction.y*(position.y-sphere.point.y)+direction.z*(position.z-sphere.point.z))
+                            c = ((sphere.point.x-position.x)**2+(sphere.point.y-position.y)**2+(sphere.point.z-position.z)**2)-sphere.rayon**2
+                            det = b**2-(4*a*c)
+                            if det>0:
+                                t1 = (-b+sqrt(det))/(2*a)
+                                t2 = (-b-sqrt(det))/(2*a)
+                                if abs(t1)>abs(t2):
+                                    t1=t2
+                                find=t1
+                            elif det==0:
+                                find=-b/(2*a)
+                            if find!=False:
+                                p = Point(((position.x+direction.x*find)-sphere.point.x)**2, ((position.y+direction.y*find)-sphere.point.y)**2,((position.z+direction.z*find)-sphere.point.z)**2)
+                                if pointMin==False:
+                                    normeMin=sqrt((p.x-position.x)**2+(p.y-position.y)**2+(p.z-position.z)**2)
+                                    pointMin=p
+                                    colorMin=sphere.color
+                                else:
+                                    norme=sqrt((p.x-position.x)**2+(p.y-position.y)**2+(p.z-position.z)**2)
+                                    if norme<normeMin:
+                                        normeMin=norme
+                                        pointMin=p
+                                        colorMin=sphere.color
+                        if pointMin!=False:
+                            self.buffer[i,j]=colorMin.getTuple()
+            
+    def process2(self):
+        test=1
                     
-    def draw(self):
-        f=open("test2.jpg",'w')
-        image = Image.new("RGB", (self.camera.screen_width, self.camera.screen_height))
-        pix = image.load()
-        for x in range(self.camera.screen_width):
-            for y in range(self.camera.screen_height):
-                pix[x,y]=self.buffer[x*self.camera.screen_width+y]
-        image.save(f, "JPEG")
-        f.close()
+    def draw(self,name):
+        file=open(name,'w')
+        self.image.save(file, "JPEG")
+        file.close()
                     
         
 
     
-scene = Scene(Camera(500,500,500,500,1))
-sphere = Sphere(Point(200,200,0), 50, Color(255,0,0))
-scene+sphere
+scene = Scene(Camera(500,500,500,500,1000))
+scene+Sphere(Point(250,250,0), 50, Color(0,0,255))
+scene+Sphere(Point(200,250,10), 100, Color(255,0,0))
+scene+Light(Point(10,20,5))
 scene.process()
-scene.draw()
+scene.draw("test2.jpg")
 
